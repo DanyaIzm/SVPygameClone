59266b809aefaff521fc51d817b4c0225bdd331c
Daniel <danizmd8@gmail.com>
2022-10-28 12:44:29 +0300

    [Update] Chapter 1 completed

--- src/code/main.py
+++ src/code/main.py
@@ -2,15 +2,19 @@ import pygame, sys
 from settings import *
 from level import Level
 
+
 class Game:
+	""" Main (game) class """
 	def __init__(self):
+		""" Setup the game """
 		pygame.init()
 		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-		pygame.display.set_caption('Sprout land')
+		pygame.display.set_caption('Voidriver Valley')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
 
 	def run(self):
+		""" Run the game """
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
@@ -21,6 +25,8 @@ class Game:
 			self.level.run(dt)
 			pygame.display.update()
 
+
 if __name__ == '__main__':
+	""" Entry point """
 	game = Game()
 	game.run()
